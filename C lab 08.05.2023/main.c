ff56188f5b82648c4f6fe0af1a9ee3faedce0e74
Chamoda R M J <132427481+Jayanichamoda@users.noreply.github.com>
2023-06-17 12:59:44 +0530

    Add files via upload

--- C lab 08.05.2023/main.c
+++ C lab 08.05.2023/main.c
@@ -2,86 +2,36 @@
 #include <stdlib.h>
 
 int main()
-{   //practical 1
+{
+    //Practical 2
+
     //Q 1
-    printf("My name is Jayani Chamoda\n");
-    printf("My school is Anula Vidyalaya\n");
+    int age;
+    printf("HI, HOW OLD ARE YOU ? ");
+    scanf("%d",&age);
+    printf("\n\nWELCOME (%d)\n",age);
+    printf("LET'S BE FRIENDS!");
 
     //Q 2
-    printf("*\n");
-    printf("**\n");
-    printf("***\n");
-    printf("****\n");
-    printf("*****\n");
+    printf("%5d%5d%5d\n",2,4,8);
+    printf("%5d%5d%5d\n",3,9,27);
+    printf("%5d%5d%5d",4,16,64);
 
     //Q 3
-    int a;
-    float b;
-    double c;
-    char name [20];
-
-    printf("Enter your name :");
-    scanf("%s",&name);
-    printf("Enter an integer :");
-    scanf("%d",&a);
-    printf("Enter a float :");
-    scanf("%f",&b);
-    printf("Enter a double :");
-    scanf("%lf",&c);
-    printf("Your name is %s\n",name);
-    printf("Integer = %d\n",a);
-    printf("Float = %f\n",b);
-    printf("Double = %f",c);
+    double dis,time,avs;
+    printf("Enter the distance traveled in meters :");
+    scanf("%lf ",&dis);
+    printf("Enter time in seconds :");
+    scanf("%[f ",&time);
+    avs=dis/time;
+    printf("The average speed is %f ms-1",avs);
 
     //Q 4
-     int d,e,total;
-
-    printf("Enter the first number:");
-    scanf("%d",&d);
-    printf("Enter the second number:");
-    scanf("%d",&e);
-    total = d+e;
-    printf("The total is %d",total);
-
-    //Q 5
-    float x,y,average;
-    printf("Enter the first decimal number:");
-    scanf("%f",&x);
-    printf("Enter the second decimal number:");
-    scanf("%f",&y);
-    average =(x+y)/2;
-    printf("The average is %f",average);
-
-    //Q 6
-    int byear,age;
-    char name1[20];
-    printf("Enter your name :");
-    scanf("%s",&name);
-    printf("Enter your birth year :");
-    scanf("%d",&byear);
-    age = 2023-byear;
-    printf("Your age is %d",age);
-
-    //Q 7
-   int n1,n2,n;
-   printf("Enter the first number :");
-   scanf("%d",&n1);
-   printf("Enter the second number :");
-   scanf("%d",&n2);
-   printf("Before swapping\nFirst number is %d\nSecond number is %d\n",n1,n2);
-
-   n=n1;
-   n1=n2;
-   n2=n;
-
-   printf("After swapping\nFirst number is %d\nSecond number is %d",n1,n2);
-
-
-
-
-
-
-
+    float c,f;
+    printf("Enter the temperature in Fahrenheit : ");
+    scanf("%f",&f);
+    c=(f-32)*(5.0/9);
+    printf("The temperature is %fC",c);
 
     return 0;
 }
